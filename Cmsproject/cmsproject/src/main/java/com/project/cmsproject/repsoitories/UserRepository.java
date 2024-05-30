7b06c5ec78becb798eeea1a07186ec86b0a5eb05
Nataraj011 <103993941+Nataraj011@users.noreply.github.com>
2024-05-30 21:32:20 +0530

    Add files via upload

---
+++ Cmsproject/cmsproject/src/main/java/com/project/cmsproject/repsoitories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.project.cmsproject.repsoitories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.project.cmsproject.entities.User;
+
+public interface UserRepository extends CrudRepository<User,Integer>{
+	
+	public Optional<User> findByUsername(String username);
+
+	public Boolean existsByUsername(String username);
+
+	public Boolean existsByEmail(String email);
+}
+
+
---
+++ Cmsproject/cmsproject/src/main/java/com/project/cmsproject/security/payload/response/JwtResponse.java
@@ -0,0 +1,33 @@
+package com.project.cmsproject.security.payload.response;
+
+import com.project.cmsproject.entities.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.ToString.Exclude;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class JwtResponse {
+	private String accessToken;
+	private String tokenType = "Bearer";
+	private Long id;
+	private String username;
+	private String email;
+	@Exclude
+	private Role roles;
+
+	public JwtResponse(String accessToken, Long id, String username, String email, Role roles) {
+		this.accessToken = accessToken;
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.roles = roles;
+	}
+}
