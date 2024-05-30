13e723c66ea6eb31578c7e40a960c271cea0a2a7
Nataraj011 <103993941+Nataraj011@users.noreply.github.com>
2024-05-30 21:27:58 +0530

    Delete Project directory

--- Project/cmsproject/src/main/java/com/project/cmsproject/service/FeatureService.java
+++
@@ -1,8 +0,0 @@
-package com.project.cmsproject.service;
-
-import com.project.cmsproject.entities.Features;
-
-public interface FeatureService {
-	public Features createFeature(Features feature);
-
-}
--- Project/cmsproject/src/main/java/com/project/cmsproject/service/ParameterService.java
+++
@@ -1,7 +0,0 @@
-package com.project.cmsproject.service;
-
-import com.project.cmsproject.entities.Parameter;
-
-public interface ParameterService {
-    public Parameter createParameter(Parameter parameter);
-}
