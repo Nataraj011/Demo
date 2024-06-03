c6dd50d8826ffdfdd14fe6b4b904005f73fb1fc3
Nataraj011 <103993941+Nataraj011@users.noreply.github.com>
2024-06-03 09:55:28 +0530

    Delete Cmsproject/cmsproject directory

--- Cmsproject/cmsproject/repsoitories/QuotationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.cmsproject.repsoitories;
-
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.cmsproject.entities.Quotation;
-
-public interface QuotationRepository extends JpaRepository<Quotation, Long> {
-}
